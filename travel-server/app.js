13d1b1a5ceda6f4c34ae163f8aed906d04000766
Yana Karcheuskaya <yana-03032017@mail.ru>
2020-01-27 11:42:57 +0300

    server message404

--- travel-server/app.js
+++ travel-server/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.status(200).json({
-        message: `${req.query.message}`
-    });
-    console.log(req.method ,req.originalUrl, ` param:${req.query.message}`);
+app.get('/echo', (req, res) => {
+    if(req.path === '/echo') {
+        res.status(200).json({
+            message: `${req.query.message}`
+        });
+        console.log(req.method, req.path, ` param:${req.query.message}`);
+    }
+});
+
+app.get('*', function(req, res){
+    res.send(`page ${req.path} not found`, 404);
 });
 
 module.exports = app;
