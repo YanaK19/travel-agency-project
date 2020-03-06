66158d3e17092b102ae05fa41dc5e5b50a7ea1f7
Yana Karcheuskaya <yana-03032017@mail.ru>
2020-03-07 00:17:42 +0300

    load account page data from server

--- travel-client/src/app/layouts/site-layout/pages/account/account.component.ts
+++ travel-client/src/app/layouts/site-layout/pages/account/account.component.ts
@@ -1,50 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from '../../../../services/user.service';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 import {UserData} from '../../../../interfaces/user/userData.interface';
 import {ReviewService} from '../../../../services/review.service';
 import {OrderService} from '../../../../services/order.service';
+import {ToursService} from '../../../../services/tours.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-account-page',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss']
 })
-export class AccountComponent implements OnInit {
+export class AccountComponent implements OnInit, OnDestroy {
    userData: any;
    toggler = true;
   isMyAccount = false;
   subscribtionsData = [];
   reviewsData = [];
   ordersData = [];
+  orderedToursData = [];
+  subscriptions: Subscription = new Subscription();
+  orderedToursLeft = [];
+  orderedToursRight = [];
+  iter = 1;
 
   constructor(private userService: UserService,
               private route: ActivatedRoute,
               private reviewService: ReviewService,
-              private  orderService: OrderService) { }
+              private  orderService: OrderService,
+              private tourService: ToursService,
+              private router: Router) { }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+
+    this.subscribtionsData = [];
+    this.reviewsData = [];
+  }
 
   ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
+    this.subscriptions.add(this.route.params.subscribe((params: Params) => {
       const user = this.userService.getUserData();
 
-      if (user._id === params.id) {
+      if (user._id == params.id) {
         this.isMyAccount = true;
         this.userData = user;
-        console.log('my account');
+        console.log('my account', this.isMyAccount);
         this.loadPageData();
       } else {
         this.userService.getUserById(params.id).subscribe((data => {
           this.userData = data;
           console.log('not my account');
+          this.isMyAccount = false;
           this.loadPageData();
         }));
       }
-    });
+    }));
   }
 
   loadPageData() {
     console.log(this.userData);
-
+    this.subscribtionsData = [];
     this.userData.subscriptions.forEach((userId) => {
       this.userService.getUserById(userId).subscribe((data => {
         this.subscribtionsData.push(data);
@@ -54,18 +71,41 @@ export class AccountComponent implements OnInit {
 
     this.reviewService.getReviewsByUserId(this.userData._id).subscribe(reviews => {
       this.reviewsData = reviews;
-      console.log(this.reviewsData);
+    /*  console.log(this.reviewsData);*/
     });
 
-    if (this.isMyAccount) {
+/*    if (this.isMyAccount) {*/
       this.orderService.getOrdersByUserId(this.userData._id).subscribe(orders => {
         this.ordersData = orders;
-        console.log(this.ordersData);
+
+        this.orderedToursData= [];
+        this.orderedToursLeft= [];
+        this.orderedToursRight= [];
+        this.iter = 1;
+
+        if (orders.length) {
+          orders.forEach(order => {
+            this.tourService.getOneTour(order.tourId).subscribe(tour => {
+              this.orderedToursData.push(tour);
+              if (this.iter % 2 === 0) {
+                this.orderedToursLeft.push(tour);
+              } else {
+                this.orderedToursRight.push(tour);
+              }
+              this.iter++;
+            });
+          });
+        }
       });
-    }
+ /*   }*/
   }
 
   toggle() {
     this.toggler = !this.toggler;
+  }
+
+  renderUserAccountPage(userAccount) {
+    console.log(userAccount)
+    this.router.navigate(['/account/' + userAccount._id]);
   }
 }
--- travel-client/src/app/layouts/site-layout/site-layout.component.ts
+++ travel-client/src/app/layouts/site-layout/site-layout.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthorizationService} from '../../services/authorization.service';
 import {Router} from '@angular/router';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-site-layout',
@@ -11,7 +12,8 @@ export class SiteLayoutComponent implements OnInit {
   userId = '';
 
   constructor(private auth: AuthorizationService,
-              private router: Router) {
+              private router: Router,
+              private userService: UserService) {
   }
 
   ngOnInit() {
@@ -27,6 +29,6 @@ export class SiteLayoutComponent implements OnInit {
   }
 
   gotoAccountPage() {
-    this.router.navigate(['/account/' + this.userId]);
+    this.router.navigate(['/account/' + this.userService.getUserData()._id]);
   }
 }
